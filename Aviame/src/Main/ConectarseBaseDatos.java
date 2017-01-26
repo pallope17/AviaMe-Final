780c8009672c5fdfc18b8b2824923a9cd63c0331
dam 2 <dam2@aulacampus.com>
2017-01-26 13:51:28 +0100

    Subo proyecto AVIAME

--- Aviame/src/Main/ConectarseBaseDatos.java
+++ Aviame/src/Main/ConectarseBaseDatos.java
@@ -1,5 +1,19 @@
 package Main;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import models.SessionFactoryUtil;
+
 public class ConectarseBaseDatos {
 
+	public static Session conectar(){
+		
+		SessionFactory sesion=SessionFactoryUtil.getSessionFactory();
+		Session session=sesion.openSession();
+		session.beginTransaction();
+		
+		return session;
+		
+	}
 }
